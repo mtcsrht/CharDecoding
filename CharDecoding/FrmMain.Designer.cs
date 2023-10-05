9c6bd7b859d90bd7e33949d2a89c19d852424d5b
Mate <mate@localhost.hu>
2023-10-05 14:51:40 +0200

    Add project files.

---
+++ CharDecoding/FrmMain.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace CharDecoding
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pnlMatrixContainer = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Karakter:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(134, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pnlMatrixContainer
+            // 
+            this.pnlMatrixContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlMatrixContainer.AutoSize = true;
+            this.pnlMatrixContainer.Location = new System.Drawing.Point(13, 104);
+            this.pnlMatrixContainer.Name = "pnlMatrixContainer";
+            this.pnlMatrixContainer.Size = new System.Drawing.Size(242, 137);
+            this.pnlMatrixContainer.TabIndex = 2;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(267, 253);
+            this.Controls.Add(this.pnlMatrixContainer);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel pnlMatrixContainer;
+    }
+}
+
---
+++ CharDecoding/FrmMain.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharDecoding
+{
+    public partial class FrmMain : Form
+    {
+
+        public Label[,] LblMatrix { get; set; }
+        public FrmMain()
+        {
+            InitializeComponent();
+            InitLblMatrix();
+        }
+
+        private void InitLblMatrix()
+        {
+            LblMatrix = new Label[7, 4];
+            for (int r = 0; r < LblMatrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < LblMatrix.GetLength(1); c++)
+                {
+                    LblMatrix[r, c] = new Label()
+                    {
+                        AutoSize = false,
+                        Bounds = new Rectangle(
+                            x: c * 50,
+                            y: r * 50,
+                            width: 50,
+                            height: 50
+                            ),
+                        BackColor = Color.White,
+                        Text = "0",
+                        Font = new Font("Arial", 24f, FontStyle.Bold),
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Margin = new Padding(5),
+                        Padding = Padding.Empty
+                    };
+                    LblMatrix[r, c].Click += LblMatrixCell_Click;
+                    pnlMatrixContainer.Controls.Add(LblMatrix[r, c]);
+                }
+            }
+        }
+
+        private void LblMatrixCell_Click(object sender, EventArgs e)
+        {
+            var lblBtn = (sender as Label);
+            if (lblBtn.Text == "0")
+            {
+                lblBtn.BackColor = Color.LightGray;
+                lblBtn.Text = "1";
+            }
+            else
+            {
+                lblBtn.BackColor = Color.White;
+                lblBtn.Text = "0";
+            }
+        }
+    }
+}
